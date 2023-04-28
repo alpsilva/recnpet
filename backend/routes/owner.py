a10b856ed4afdafd9e8a6b5799c919ea81335849
André Luiz <alps2@cin.ufpe.br>
2023-04-28 16:30:30 -0300

    Adding vaccines history

--- backend/routes/owner.py
+++ backend/routes/owner.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
 from datetime import datetime
+from pydantic import BaseModel
 from .controller import db
+import time
 
 router = APIRouter(
     prefix="/owner",
@@ -26,6 +27,12 @@ class NewPetInput(BaseModel):
     fur: str
     castrated: bool
     description: str
+
+class NewVaccineInput(BaseModel):
+    name: str
+    vaccine_id: str
+    applied_at: str
+    valid_until: str
 
 @router.put("/", status_code=200)
 async def register_owner(request: NewOwnerInput):
@@ -53,10 +60,15 @@ async def register_pet(owner_id: str, request: NewPetInput):
 
     new_pet = request.dict()
     new_pet["register_date"] = str(datetime.now())
+    if new_pet["chip"] == "generate":
+        new_pet["chip"] = time.time_ns()
 
     current_data = db.get_data_by_key("owners", owner_id, 1)
     for _, value in current_data.items():
         current_pets = value.get("pets", [])
+
+    if type(current_pets) == dict:
+        current_pets = list(current_pets.values())
 
     pets = current_pets + [new_pet]
 
@@ -67,3 +79,31 @@ async def register_pet(owner_id: str, request: NewPetInput):
     )
 
     return { "pets": pets }
+
+@router.post("/{owner_id}/vaccine/{pet_id}/", status_code=200)
+async def register_vaccine(owner_id: str, pet_id: str, request: NewVaccineInput):
+    """ Inserts a vaccine register to the owner's pet. """
+
+    new_vaccine = request.dict()
+    new_vaccine["register_date"] = str(datetime.now())
+
+    current_data = db.get_data_by_key("owners", owner_id, 1)
+    for _, value in current_data.items():
+        current_pets = value.get("pets", [])
+
+    if type(current_pets) == dict:
+        current_pets = list(current_pets.values())
+
+    for pet in current_pets:
+        if pet["chip"] == pet_id:
+            if "vaccines" not in pet:
+                pet["vaccines"] = []
+            pet["vaccines"].append(new_vaccine)
+
+    db.update_data_by_key(
+        "owners",
+        owner_id,
+        { "pets": current_pets }
+    )
+
+    return { "pets": current_pets }
