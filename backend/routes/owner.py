2412945c6c671c6fe18363cca3feec3284a13650
André Luiz <alps2@cin.ufpe.br>
2023-04-28 15:03:21 -0300

    Chaning owner collection name to owners

--- backend/routes/owner.py
+++ backend/routes/owner.py
@@ -29,7 +29,7 @@ async def register_owner(request: NewOwnerInput):
     new_owner = request.dict()
     new_owner["register_date"] = str(datetime.now())
 
-    response = db.push_data("owner", new_owner)
+    response = db.push_data("owners", new_owner)
     if response is None:
         raise HTTPException(status_code=500, detail="Something went wrong while inserting data.")
     return { "key": response.key }
@@ -37,7 +37,7 @@ async def register_owner(request: NewOwnerInput):
 @router.get("/{cpf}/", status_code=200)
 async def get_owner_by_cpf(cpf: str):
     """ Returns the owner whose cpf is the same as the given one. """
-    owner = db.get_data_by_value("owner", "cpf", cpf, 1)
+    owner = db.get_data_by_value("owners", "cpf", cpf, 1)
     if len(owner) == 0:
         raise HTTPException(status_code=404, detail="Owner not found.")
     for key, value in owner.items():
@@ -50,14 +50,14 @@ async def register_pet(owner_id: str, request: NewPetInput):
     new_pet = request.dict()
     new_pet["register_date"] = str(datetime.now())
 
-    current_data = db.get_data_by_key("owner", owner_id, 1)
+    current_data = db.get_data_by_key("owners", owner_id, 1)
     for _, value in current_data.items():
         current_pets = value.get("pets", [])
 
     pets = current_pets + [new_pet]
 
     db.update_data_by_key(
-        "owner",
+        "owners",
         owner_id,
         { "pets": pets }
     )
