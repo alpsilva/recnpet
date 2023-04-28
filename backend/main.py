6230f31e9af3ff2a3a349045bee59c93ca7b388a
André Luiz <alps2@cin.ufpe.br>
2023-04-27 23:48:43 -0300

    db connection

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from db_connection import DbConnection
 
 app = FastAPI()
+
+db = DbConnection()
 
 @app.get("/")
 async def root():
     return {"message": "aaaaaaaaaaaaaaaa World"}
+
+@app.put("/owner/")
+async def register_owner():
+    new_owner = {
+        "name": "Pedrin Maldade Pura",
+        "Catiorros": [
+            {
+                "name": "zezin ruim de mira"
+            }
+        ]
+    }
+    db.push_data("dono_cachorro", new_owner)
+    return 200
