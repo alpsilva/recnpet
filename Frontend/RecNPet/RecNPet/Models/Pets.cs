d32f06983124cfcc9a9d1c5055a3426d6d9965b4
koga25 <66087274+koga25@users.noreply.github.com>
2023-04-28 21:38:12 -0300

    create PetDetails page

--- Frontend/RecNPet/RecNPet/Models/Pets.cs
+++ Frontend/RecNPet/RecNPet/Models/Pets.cs
@@ -6,5 +6,8 @@
         public string species;
         public string years;
         public string? image;
+        public string race;
+        public string sex;
+        public bool castrated;
     }
 }
